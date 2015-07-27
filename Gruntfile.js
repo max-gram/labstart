39be2f0b7bf435447700a388eed54a10fae8fc84
Max Gram <gramotkin@gmail.com>
2015-07-27 16:15:42 -0400

    initial

--- Gruntfile.js
+++ Gruntfile.js
@@ -143,7 +143,6 @@ module.exports = function(grunt) {
   });
 
   grunt.registerTask('default',[
-    'check-modules',
     'licensechecker',
     'clean:dev',
     'copy:dev',
--- app/sections/landing/index.js
+++ app/sections/landing/index.js
@@ -1,23 +1,8 @@
-var Swiper = require('../../com/libs/swiper/swiper.js');
+
 
 var LandingPage = {
   init: function(){
-    this.setSwiper();
-  },
-
-  setSwiper: function(){
-    var mySwiper = new Swiper ('.swiper-container', {
-      // Optional parameters
-      direction: 'horizontal',
-      loop: true,
-
-      // If we need pagination
-      pagination: '.swiper-pagination',
-
-      // Navigation arrows
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-    });
+    console.log('Index Init')
   }
 };
 

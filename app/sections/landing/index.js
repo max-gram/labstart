b6575799f9780bb2229925035267d7b94353a135
Max Gram <dev@maxgram.com>
2015-10-20 13:34:17 -0400

    scrollmagic

--- app/sections/landing/index.js
+++ app/sections/landing/index.js
@@ -1,18 +1,37 @@
-var Swiper = require('../../com/libs/swiper/swiper.js');
+var ScrollMagic = require('scrollmagic');
+var smPluginTween = require('../../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap.js');
+var smPluginTween = require('../../../node_modules/scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators.js');
+console.log(this.cwd)
+var TweenMax = require('gsap');
 
 var LandingPage = {
   init: function(){
-    this.setSwiper();
+    this.setParallax();
   },
 
-  setSwiper: function(){
-    var mySwiper = new Swiper ('.swiper-container', {
-      direction: 'horizontal',
-      loop: true,
-      pagination: '.swiper-pagination',
-      nextButton: '.swiper-button-next',
-      prevButton: '.swiper-button-prev',
-    });
+  setParallax: function(){
+    var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "100%"}});
+
+    // build scenes
+    new ScrollMagic.Scene({triggerElement: "#screen2"})
+    .setTween("#screen2", {y: "-90%", ease: Linear.easeNone})
+    .addIndicators()
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#screen3"})
+    .setTween("#screen3", {y: "-180px", ease: Linear.easeNone})
+    .addIndicators()
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#screen4"})
+    .setTween("#screen4", {y: "-60%", ease: Linear.easeNone})
+    .addIndicators()
+    .addTo(controller);
+
+    new ScrollMagic.Scene({triggerElement: "#screen5"})
+    .setTween("#screen5", {y: "-30%", ease: Linear.easeNone})
+    .addIndicators()
+    .addTo(controller);
   }
 };
 
